ed3cdf3103469480a882b99978260b931c30fcf6
Lenovo <Lenovo@LAPTOP-KH5RR2B5>
2024-02-09 09:50:02 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Kata.kyu.kyu6;
+using System;
+
+namespace Kata
+{
+    public static class Kata
+    {
+        public static void Main()
+        {
+            //Console.WriteLine(PrinterError("kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"));
+            //Console.ReadKey();
+            Console.WriteLine(TitleCase.GetCase("aBC deF Ghi", null));
+            Console.ReadKey();
+        }
+
+        //public static string PrinterError(String s)
+        //{
+        //    // your codes
+        //    Dictionary<char, int> duplicates = new Dictionary<char, int>();
+
+        //    foreach (char c in s)
+        //    {
+        //        if (duplicates.ContainsKey(c) && (int)c>=97 && (int)c<=109)
+        //        {
+        //            duplicates[c] += 1;
+        //        }
+        //        else if((int)c >= 97 && (int)c <= 109)
+        //            duplicates[c] = 1;
+        //    }
+
+        //    return duplicates.Count + "/" + s.Length;
+        //}
+    }
+}
---
+++ kyu/kyu6/TitleCase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.kyu.kyu6
+{
+    public static class TitleCase
+    {
+        public static string GetCase(string title, string minorWords = "")
+        {
+            if (title.Length > 1)
+            {
+                if (minorWords == null) minorWords = string.Empty;
+
+                string[] words = new string[title.Split().Length];
+                string result = string.Empty;
+                bool first_word = true;
+                int index = 0;
+                foreach (string sent in (title.ToLower()).Split())
+                {
+                    bool found = false;
+                    foreach (string word in (minorWords.ToLower()).Split())
+                    {
+                        if (first_word)
+                        {
+                            first_word = false;
+                            found = false;
+                            break;
+                        }
+                        else if (sent == word)
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found)
+                    {
+                        words[index] += char.ToUpper(sent[0]) + sent.Substring(1);
+                    }
+                    else
+                    {
+                        words[index] += sent;
+                    }
+                    index++;
+                }
+                Console.WriteLine(String.Join(" ", words));
+                return String.Join(" ", words);
+            }
+            else return "";
+        }
+    }
+}
