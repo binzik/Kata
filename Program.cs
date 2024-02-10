286fc2d41ce3e3cf8353c8ef4d9ed76bca697b4b
binzik <roguemaks@gmail.com>
2024-02-10 16:58:21 +0100

    A test selection menu has been created

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Kata.kyu.kyu6;
 using Kata.kyu.kyu7;
 using System;
+using System.Runtime.CompilerServices;
 
 namespace Kata
 {
@@ -8,37 +9,59 @@ namespace Kata
     {
         public static void Main()
         {
-            //Console.WriteLine(PrinterError("kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"));
-            //Console.ReadKey();
-            //Console.WriteLine(TitleCase.GetCase("aBC deF Ghi", null));
-
-            //Node node = null;
-            //node = Node.Push(node, 3);
-            //node = Node.Push(node, 2);
-            //node = Node.Push(node, 1);
-
-            //Console.WriteLine(node);
-
-            ArrayDif.ArrayDiff(new int[] {1, 2, 2, 2, 3}, new int[] {2});
-            Console.ReadKey();
+            ShowMenu();
         }
 
-        //public static string PrinterError(String s)
-        //{
-        //    // your codes
-        //    Dictionary<char, int> duplicates = new Dictionary<char, int>();
+        static void ShowMenu()
+        {
+            bool exit = true;
+            while (exit)
+            {
+                Console.WriteLine("*** Menu ***");
+                Console.WriteLine("1. Kyu 8");
+                Console.WriteLine("2. Kyu 7");
+                Console.WriteLine("3. Kyu 6");
 
-        //    foreach (char c in s)
-        //    {
-        //        if (duplicates.ContainsKey(c) && (int)c>=97 && (int)c<=109)
-        //        {
-        //            duplicates[c] += 1;
-        //        }
-        //        else if((int)c >= 97 && (int)c <= 109)
-        //            duplicates[c] = 1;
-        //    }
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("Chose Lvl: ");
 
-        //    return duplicates.Count + "/" + s.Length;
-        //}
+
+                switch (Int32.Parse(Console.ReadLine()))
+                {
+                    case 0:
+                        exit = false;
+                        break;
+                    case 1:
+                        Console.WriteLine("Chosen kyu 8");
+                        break;
+                    case 2:
+                        Console.WriteLine("Chosen kyu 7");
+                        break;
+                    case 3:
+                        Console.WriteLine("Chosen kyu 6");
+                        break;
+                    default:
+                        Console.WriteLine("Chosen incorrectly");
+                        break;
+                }
+            }
+        }
+
+        static void ShowMenuKyu8()
+        {
+            while (true)
+            {
+                Console.WriteLine("*** Menu ***");
+                Console.WriteLine("1. Kyu 8");
+            }
+        }
+
+        static void TestExamples()
+        {
+            void BouncingBallTest()
+            {
+                BouncingBall.bouncingBall(3.0, 0.66, 1.5);
+            }
+        }
     }
 }
---
+++ kyu/kyu6/BouncingBall.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.kyu.kyu6
+{
+    public class BouncingBall
+    {
+
+        public static int bouncingBall(double h, double bounce, double window)
+        {
+            // your code
+            int seen = 0;
+            double ballLocation = h;
+            if (h > 0 && bounce > 0 && bounce < 1 && window < h)
+            {
+                while (h < window)
+                {
+                    if(true) { seen++; }
+                }
+
+            }
+            else return -1;
+            return 0;
+        }
+    }
+}
