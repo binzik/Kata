08d503b0c8bb84c7d854739f09865644321e5430
binzik <roguemaks@gmail.com>
2024-02-15 22:26:56 +0100

    #Digitizer

---
+++ kyu/kyu8/Digitizer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.kyu.kyu8
+{
+    class Digitizer
+    {
+        public static long[] Digitize(long n)
+        {
+            string numberString = n.ToString();
+            long[] digits = new long[0];
+
+
+            return digits;
+        }
+    }
+}
