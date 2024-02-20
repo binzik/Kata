5c73ff554619e044fd181bbce71edbe034cc10bf
Binzik <roguemaks@gmail.com>
2024-02-20 17:05:39 +0100

    #FindIt Create

---
+++ kyu/kyu6/FindIt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.kyu.kyu6
+{
+    internal class FindIt
+    {
+        public static int find_it(int[] seq)
+        {
+            Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+            foreach(int i in seq)
+            {
+
+            }
+            return -1;
+        }
+    }
+}
