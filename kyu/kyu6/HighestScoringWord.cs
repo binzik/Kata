606d2cdb2771eb4f9d49a607ff01dcd4ad639d97
Binzik <roguemaks@gmail.com>
2024-02-22 14:31:56 +0100

    #HighestScoringWord Created

---
+++ kyu/kyu6/HighestScoringWord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.kyu.kyu6
+{
+    internal class HighestScoringWord
+    {
+        public static string High(string s)
+        {
+            return s;
+        }
+    }
+}
