760b5419dc94c3a0e69a4528838073f6656331df
binzik <roguemaks@gmail.com>
2024-03-11 23:51:11 +0100

    #StonePickAxe Created

---
+++ kyu/kyu7/StonePickAxe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.kyu.kyu7
+{
+    //Course: https://www.codewars.com/kata/65c0161a2380ae78052e5731/train/csharp
+    //Status: In Progress
+    internal class StonePickAxe
+    {
+        public static int StonePick(string[] arr)
+        {
+            return 0;
+        }
+    }
+}
