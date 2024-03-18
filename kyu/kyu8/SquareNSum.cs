d78ea2637585b38865ed8ba76e82be16c41a4e6b
binzik <roguemaks@gmail.com>
2024-03-18 22:52:09 +0100

    #SquareNSum Created

---
+++ kyu/kyu8/SquareNSum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.kyu.kyu8
+{
+    internal class SquareNSum
+    {
+        public static int SquareSum(int[] numbers)
+        {
+            int result = 0;
+            foreach (int number in numbers)
+            {
+                result += number*number;
+            }
+            return result;
+        }
+    }
+}
