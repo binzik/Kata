6ad38e1c13005b3a6c80e87d667162e2d55ecf81
binzik <roguemaks@gmail.com>
2024-03-27 23:43:07 +0100

    #ReverseWords Created

---
+++ kyu/kyu7/ReverseWords.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.kyu.kyu7
+{
+    //Course: https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/train/csharp
+    //Status: In Progress
+    internal class ReverseWords
+    {
+        public static string GetTestDescription()
+        {
+            return "";
+        }
+        public static string ReverseWord(string str)
+        {
+            string result = str;
+            //Divide into elementss
+            string[] words = result.Split(' ');
+
+            return result;
+        }
+    }
+}
