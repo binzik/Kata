4b3e7529d0c64c8cc59e08680ed5b64c3953cf74
binzik <roguemaks@gmail.com>
2024-05-13 22:39:16 +0200

    #Testing123 Created Main method, description method

---
+++ kyu/kyu7/Testing123.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.kyu.kyu7
+{
+    //Course: https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/csharp
+    //Status: In Progress
+    internal class Testing123
+    {
+        public static string GetTestDescription()
+        {
+            return "Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.\r\n\r\nWrite a function which takes a list of strings and returns each line prepended by the correct number.\r\n\r\nThe numbering starts at 1. The format is n: string. Notice the colon and space in between.\r\n\r\nExamples: (Input --> Output)\r\n\r\n[] --> []\r\n[\"a\", \"b\", \"c\"] --> [\"1: a\", \"2: b\", \"3: c\"]";
+        }
+        public static List<string> Number(List<string> lines)
+        {
+            //your code goes here
+            return lines;
+        }
+    }
+}
