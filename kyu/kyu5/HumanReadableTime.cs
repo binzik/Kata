4996717cf41bfc4a900f4dec9a9718db589d9e46
binzik <roguemaks@gmail.com>
2024-06-02 23:26:47 +0200

    #HumanReadableTime In progress

---
+++ kyu/kyu5/HumanReadableTime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.kyu.kyu5
+{
+    //Course: https://www.codewars.com/kata/52685f7382004e774f0001f7/train/csharp
+    //Status: In Progress
+    public class HumanReadableTime
+    {
+        public static string GetReadableTime(int seconds)
+        {
+            int hourInSecounds, minuteInSeconds;
+            //Count hours
+            hourInSecounds = (seconds-(seconds % (60 * 60)));
+            seconds -= hourInSecounds;
+            //Count minutes
+            minuteInSeconds = seconds - (seconds % 60);
+            seconds-= minuteInSeconds;
+            
+
+            return $"{hourInSecounds}:{minuteInSeconds}:{seconds}";
+        }
+    }
+}
