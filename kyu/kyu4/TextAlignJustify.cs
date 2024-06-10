9c09a1d56006d0466338a11f7a2e2e33cbe91fe5
binzik <roguemaks@gmail.com>
2024-06-10 22:44:06 +0200

    #TextAlignJustify Created

---
+++ kyu/kyu4/TextAlignJustify.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.kyu.kyu4
+{
+    internal class TextAlignJustify
+    {
+        public static string Justify(string str, int len)
+        {
+            return "";
+        }
+    }
+}
