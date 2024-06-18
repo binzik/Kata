f459e415abeaa08f2c837e62c26523b64c64c645
Binzik <roguemaks@gmail.com>
2024-06-18 15:46:06 +0200

    #Persist Truly Completed

--- kyu/kyu6/Persist.cs
+++ kyu/kyu6/Persist.cs
@@ -7,32 +7,23 @@ using System.Threading.Tasks;
 namespace Kata.kyu.kyu6
 {
     //Course: https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/csharp
-    //Status: In Progress
+    //Status: Completed
     public class Persist
     {
         public static int Persistence(long n)
         {
-            //if(n < 10) return 0;
-            //int multiplications = 0;
-            //while (n > 9)
-            //{
-            //    long worker = 1;
-            //    for(int i = 0; i< n.ToString().Length; i++)
-            //    {
-            //        if((long)n.ToString()[i] - 48==0) continue;
-            //        //long c = (long)n.ToString()[i] - 48;
-            //        worker = worker * ((long)n.ToString()[i] - 48);
-            //    }
-            //    multiplications++;
-            //    n = worker;
-            //}
-            //return multiplications;
-
             if(n<10) return 0;
-            int multiplications = 1;
+            int multiplications = 0;
             while (n>=10)
             {
-                
+                long r = 1;
+                for(int i = 0; i < n.ToString().Length; i++)
+                {
+                    r*= n.ToString().ToArray()[i]-48;
+                }
+
+                n = r;
+                multiplications++;
             }
 
             return multiplications;
