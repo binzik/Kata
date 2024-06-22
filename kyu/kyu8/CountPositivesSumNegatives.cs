56dfb92e1f6575b63dea2fbb40ad3609d12653f7
Binzik <roguemaks@gmail.com>
2024-06-22 08:14:46 +0200

    #CountPositivesSumNegatives Created and Completed

---
+++ kyu/kyu8/CountPositivesSumNegatives.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.kyu.kyu8
+{
+    //Course: https://www.codewars.com/kata/576bb71bbbcf0951d5000044/train/csharp
+    //Status: Complete
+    public class CountPositivesSumNegatives
+    {
+        public static int[] CountPositivesAndSumNegatives(int[] input)
+        {
+            if(input == null || input.Length == 0)
+            {
+                return new int[] {};
+            }
+            return new int[] { CountPositives(input), SumNegatives(input) };
+        }
+
+        public static int CountPositives(int[] input) => input.Where(x => x > 0).Count();
+
+        public static int SumNegatives(int[] input) => input.Where(x => x < 0).Sum();
+    }
+}
