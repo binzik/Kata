f274562db59e220edbc35be54ff165616fe91026
Binzik <roguemaks@gmail.com>
2024-06-25 11:31:47 +0200

    #TextAlignJustify Complete

--- kyu/kyu4/TextAlignJustify.cs
+++ kyu/kyu4/TextAlignJustify.cs
@@ -8,28 +8,28 @@ using System.Threading.Tasks;
 namespace Kata.kyu.kyu4
 {
     //Copurse: https://www.codewars.com/kata/537e18b6147aa838f600001b/train/csharp
-    //Status: In Progress
+    //Status: Complete
     public class TextAlignJustify
     {
         public static string Justify(string str, int len)
         {
+            //Check if input: str null or empty
+            if(str == null || str == string.Empty)
+            {
+                return string.Empty;
+            }
+            Console.WriteLine(str);
+            
             string result = string.Empty;
             //PLAN
             //Split to words
             List<string> list = str.Split(' ').ToList();
-
             //Check if one or no word
-             if(list.Count == 1)
-             {
-                return str + "\n"; ;
-             }
-            else if(list.Count==0)
-            {
-                return "\n";
-            }
+            if (list.Count == 1) return str + "\n";
+
 
             //Split words to lane
-            List<string> lines = new List<string>();
+            List<List<string>> lines = new List<List<string>>();
             List<string> line = new List<string>();
             int lineLength = 0;
 
@@ -40,24 +40,36 @@ namespace Kata.kyu.kyu4
                 //If yes add to list of words to new line and clear list
                 //if no add to list of words
                 //
-                if (lineLength+s.Length+line.Count < len)
+                if (lineLength + s.Length + line.Count > len)
+                {
+                    lines.Add(line);
+                    lineLength = 0;
+                    line = new List<string>();
+                }
+                if (lineLength + s.Length + line.Count <= len)
                 {
                     line.Add(s);
                     lineLength += s.Length;
                 }
 
-
-                
-                
             }
-            //calculate spaces
-            //stick to one output
-            //
+            //Check if any element in line list if yes add to the lines List
+            if(line.Count > 0)
+            {
+                lines.Add(line);
+            }
+            foreach(List<string> strings in lines)
+            {
+                //Length of line
+                int wordsLength = strings.Sum(s => s.Length);
 
+                if (strings == lines.Last())
+                    result += string.Join(" ", strings);
+                else
+                    result += JustifyLine(strings, wordsLength, len)+"\n";
+            }
 
-
-
-            return result+"\n";
+            return result;
         }
 
         public static string JustifyLine(List<string> words, int currentLineLength, int width)
